631b1f96666b16cfe9fe7b8cd3dec0ff9aa005e4
SalisMaxima <mathiashl@pm.me>
2026-04-01 06:58:47 +0200

    upload step15

--- scripts/hpc/upload_step15k.py
+++ scripts/hpc/upload_step15k.py
@@ -2,13 +2,19 @@
 
 import wandb
 
-api = wandb.Api()
-run = api.run("mathiashl-danmarks-tekniske-universitet-dtu/danish-asr/gi46t3kp")
-artifact = wandb.Artifact(
-    name="omniasr-ctc-danish-gi46t3kp",
-    type="checkpoint",
-    metadata={"step": 15000, "path": "step_15000/model/pp_00/tp_00/sdp_00.pt"},
-)
-artifact.add_file("/work3/s204696/outputs/omniasr_20k/ws_1.250158f0/checkpoints/step_15000/model/pp_00/tp_00/sdp_00.pt")
-run.log_artifact(artifact)
-print("Done")
+with wandb.init(
+    project="danish-asr",
+    entity="mathiashl-danmarks-tekniske-universitet-dtu",
+    id="gi46t3kp",
+    resume="must",
+) as run:
+    artifact = wandb.Artifact(
+        name="omniasr-ctc-danish-gi46t3kp",
+        type="checkpoint",
+        metadata={"step": 15000, "path": "step_15000/model/pp_00/tp_00/sdp_00.pt"},
+    )
+    artifact.add_file(
+        "/work3/s204696/outputs/omniasr_20k/ws_1.250158f0/checkpoints/step_15000/model/pp_00/tp_00/sdp_00.pt"
+    )
+    run.log_artifact(artifact)
+    print("Done")
